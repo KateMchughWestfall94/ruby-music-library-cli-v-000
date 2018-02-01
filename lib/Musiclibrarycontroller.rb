bd02496cbcca7ee1ee23862bcbdeab7133992c0c
Kate McHugh-Westfall <kate.mchugh94@gmail.com>
2018-02-01 22:04:08 +0000

    automatically backed up by learn

--- lib/Musiclibrarycontroller.rb
+++ lib/Musiclibrarycontroller.rb
@@ -43,10 +43,10 @@ class MusicLibraryController
   end
 
   def list_artists
-    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
-      puts "#{i}. #{a.name}"
+    Artist.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |n, i|
+      puts "#{i}. #{n.name}"
     end
-  end
+  
 
   def list_genres
     Genre.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |g, i|
